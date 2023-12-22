df39b159080885c6f543532fd0602cab744e2694
Sujeet Kumar Gupta <sujeet.2024ec1161@kiet.edu>
2023-12-22 23:00:55 +0530

    update

--- server.js
+++ server.js
@@ -1,34 +1,37 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-// import dotenv from 'dotenv';
+
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
 // Database connection
-import Connection from "./database/db.js";
+import Connection from './database/db.js';
 
 // Routes
 // import Routers from './routes/route.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
-// dotenv.config();
+dotenv.config();
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
-// const USERNAME = process.env.DB_USERNAME;
-// const PASSWORD = process.env.DB_PASSWORD;
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
 
 // DB connection
-Connection();
+Connection(USERNAME, PASSWORD);
 
 // Routes
 // app.use('/', Routers)
 
+app.get("hellow world")
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-});
+ });
